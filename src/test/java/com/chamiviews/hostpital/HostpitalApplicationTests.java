2f03637362b530d0c9337ef0f2fa094944a83311
chamodlakmal <chamodlakmal97@gmail.com>
2021-10-26 21:10:25 +0530

    login added

--- src/test/java/com/chamiviews/hostpital/HostpitalApplicationTests.java
+++ src/test/java/com/chamiviews/hostpital/HostpitalApplicationTests.java
@@ -1,13 +1,25 @@
 package com.chamiviews.hostpital;
 
+import com.chamiviews.hostpital.model.Hospital;
+import com.chamiviews.hostpital.service.HospitalImpl;
+import com.chamiviews.hostpital.service.HospitalService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class HostpitalApplicationTests {
 
+    @Autowired
+    HospitalImpl hospitalIm;
+
     @Test
     void contextLoads() {
+        List<Hospital> hospitals =hospitalIm.findByAddress("jfjjgj");
+        System.out.println(hospitals);
     }
+
 
 }
